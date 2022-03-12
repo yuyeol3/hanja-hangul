eb89ac93c75d31dbdc013b11a0d9816cafd5a3ef
yuyeol3 <zerom50@gmail.com>
2022-03-12 18:16:20 +0900

    공간복잡도 고려해 수정

--- hangle_table_generator.py
+++ hangle_table_generator.py
@@ -1,34 +1,69 @@
 import json
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import shutil
+import os
 
-kor_hanja = {}
+def get_driver():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('headless')
 
-with open("kor_unicodes.json", 'r', encoding="utf8") as f:
-    kor_letters = json.load(f)
+    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=driver_options)
+    url = "data:,"
+    driver.get(url)
+    driver.minimize_window()
 
-headers = {"User-Agent":r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}
-
-for letter in kor_letters:
-    page = 1
-    while True:
-        kor_hanja[letter] = []
-
-        url = f"https://hanja.dict.naver.com/#/search?range=letter&page={page}&query={letter}&autoConvert="
-        res = requests.get(url, headers=headers)
-        res.raise_for_status()
-
-        print(res.text)
-        soup = BeautifulSoup(res.text, 'lxml')
-        hanjas = soup.find_all("a", attrs={'class':'hanja_link'})
-
-        if len(hanjas) == 0:  # 종료조건
-            break
-
-        for hanja in hanjas:
-            kor_hanja[letter].append(hanja)
-            print(hanja)
-
-        page += 1
+    return driver
 
 
+def main():
+    dir_name = 'hangul_hanja'
+
+    shutil.rmtree(dir_name)  # 폴더와 그 폴더 안의 항목들 삭제
+    os.mkdir(dir_name)  # 경로 생성
+
+    driver = get_driver()
+    reset_count = 0
+
+    with open("kor_unicodes.json", 'r', encoding="utf8") as f:
+        kor_letters = json.load(f)
+
+
+    num_of_char = len(kor_letters)
+
+    for idx, letter in enumerate(kor_letters):
+        print(f"{letter}: {((idx + 1) / num_of_char) * 100:.2f}% 완료")
+        page = 1
+        kor_hanja = []
+        while True:
+            url = f"https://hanja.dict.naver.com/#/search?range=letter&page={page}&query={letter}&autoConvert=false"
+            driver.get(url)
+
+            sleep(1)
+            
+            hanjas = driver.find_elements_by_class_name("hanja_link")
+            hanjas = [i.text for i in hanjas]
+
+            if len(hanjas) == 0:
+                break
+
+            for hanja in hanjas:
+                kor_hanja.append(hanja)
+
+            
+            page += 1
+            reset_count += 1
+        
+        if len(kor_hanja) >= 1:
+            with open(f"hangul_hanja/{letter}.json", "w", encoding="utf8") as f:
+                json.dump(kor_hanja, f)
+
+
+        if reset_count >= 50:  # 메모리 누수 문제로 일정 횟수 이후 웹드라이버 초기화
+            driver.close()
+            driver = get_driver()
+            reset_count = 0
+
+
+if __name__ == "__main__":
+    main()
