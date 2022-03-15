3f6816b82757dcbc615f3f9231881506d79f3f37
yuyeol3 <zerom50@gmail.com>
2022-03-15 21:06:56 +0900

    커맨드라인에 진행률 표시하는 기능 추가

--- translate.py
+++ translate.py
@@ -5,7 +5,6 @@ import han_check
 
 def main():
     file_list = os.listdir("./hangul_hanja")
-    # print(file_list)
 
     with open("text_to_translate.txt", "r", encoding="utf8") as f:
         text = f.read()
@@ -21,10 +20,20 @@ def main():
         preference_table = json.load(f)  # 선호하는 한자 음변환 테이블
 
 
-    for hanja in hanja_list:
+    for idx, hanja in enumerate(hanja_list):
+
+        # 진행률 표시해주는 코드
+        percentage = ((idx + 1) / len(hanja_list)) * 100
+        percentage = int(percentage)
+        print("\r", end="")
+        print(f"{percentage:3}% ", end="")
+        for i in range(percentage): print("■", end="")
+
+        # 선호 한자음이 지정되어 있으면
         if hanja in preference_table:
             text = text.replace(hanja, preference_table[hanja])
 
+        # 아닌 경우
         else:
             for file_name in file_list:
                 with open(f"hangul_hanja/{file_name}", "r", encoding="utf8") as f:
